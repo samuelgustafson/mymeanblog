fc64b841ce06e4bd095420cfee245f8fd65aa3b9
Justin Dennison <justin1dennison@gmail.com>
2017-03-22 19:42:03 -0400

    implemented the user service that provides methods that are useful to us at this point in time

--- public/js/services/user.service.js
+++ public/js/services/user.service.js
@@ -2,7 +2,57 @@
   angular.module('mymeanblog')
         .factory('UserService', UserService);
 
-  UserService.$inject = [];
+  UserService.$inject = ['$http'];
 
-  function UserService(){}
+  function UserService($http){
+    var base = '/users';
+
+    function login(){
+      return $http.post('/login')
+                  .then(function(response){
+                    console.log(response);
+                  });
+    }
+    function signup(user){
+      return $http.post('/signup', user)
+                  .then(function(response){
+                    console.log(response);
+                  });
+    }
+    function getAll(){
+      return $http.get(base)
+                  .then(function(response){
+                    console.log(response);
+                  });
+    }
+    function getOne(user){
+      var url = `${base}/${user._id}`;
+      return $http.get(url)
+                  .then(function(response){
+                    console.log(response);
+                  });
+    }
+    function update(user){
+      var url = `${base}/${user._id}`;
+      return $http.put(url, user)
+                  .then(function(response){
+                    console.log(response);
+                  });
+    }
+    function deleteUser(user){
+      var url = `${base}/${user._id}`;
+      return $http.delete(url)
+                  .then(function(response){
+                    console.log(response);
+                  });
+    }
+    return {
+      getAll: getAll,
+      login: login,
+      getOne: getOne,
+      signup: signup,
+      update: update,
+      delete: deleteUser
+    }
+  }
 }());
