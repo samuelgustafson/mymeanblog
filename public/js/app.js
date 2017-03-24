16a8a9a5c6075c977a8de8ef92c83643a2d1552c
Justin Dennison <justin1dennison@gmail.com>
2017-03-24 19:59:10 -0400

    moved the route configuration to a separate file for organization

--- public/js/app.js
+++ public/js/app.js
@@ -3,23 +3,5 @@
     "ngRoute",
   ]);
 
-  angular.module('mymeanblog')
-      .config(RouterConfig);
-
-  RouterConfig.$inject = ['$routeProvider'];
-
-  function RouterConfig($routeProvider){
-    $routeProvider
-      .when('/', {
-        controller: 'SignupController',
-        templateUrl: 'html/views/signup.html'
-      })
-      .when('/login', {
-        controller: 'LoginController',
-        templateUrl: 'html/views/login.html'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }
+  
 }());
---
+++ public/js/routes.js
@@ -0,0 +1,21 @@
+(function() {
+  angular.module('mymeanblog')
+      .config(RouterConfig);
+
+  RouterConfig.$inject = ['$routeProvider'];
+
+  function RouterConfig($routeProvider){
+    $routeProvider
+      .when('/', {
+        controller: 'SignupController',
+        templateUrl: 'html/views/signup.html'
+      })
+      .when('/login', {
+        controller: 'LoginController',
+        templateUrl: 'html/views/login.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }  
+}());
